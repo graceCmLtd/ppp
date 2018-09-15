7e49608ef3bf1a3841c164cf424a300992bdb294
gavin <gracecmpltd@163.com>
2018-09-15 19:50:08 +0800

    练习方式

--- src/main.js
+++ src/main.js
@@ -21,10 +21,10 @@ Vue.prototype.axios=Axios;
  //Vue.prototype.oUrl='http://39.104.140.40/ppp'
 
 
-   //Vue.prototype.oUrl='http://localhost:8080/ppp'
+   Vue.prototype.oUrl='http://localhost:8080/ppp'
 
 
-Vue.prototype.oUrl='http://39.104.140.40/ppp'
+//Vue.prototype.oUrl='http://39.104.140.40/ppp'
 //Vue.prototype.oUrl='http://pengpengpiao.cn/ppp'
 
 
