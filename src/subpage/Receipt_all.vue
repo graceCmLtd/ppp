7d99b085366cec3a6c6a9760008c6bdb617a4361
gavin <gracecmpltd@163.com>
2018-09-29 15:45:53 +0800

    sdkk

--- src/subpage/Receipt_all.vue
+++ src/subpage/Receipt_all.vue
@@ -34,11 +34,11 @@
           <el-col :span="3"><div class="mes pula">
             <span>{{item.intentionStatus}}</span>
           </div></el-col>
-          <el-col :span="3" v-if="item.intentionStatus == '卖家已确认'"><div class="mes operaMes">
-            <!--<p><button type="button" name="button" @click="turnPlace(index)">交易</button></p>-->
-            <!--<p><button type="button" name="button">放弃</button></p>-->
+          <el-col :span="3" v-if="item.intentionStatus == '卖家已确认'"><!-- <div class="mes operaMes">
+            <p><button type="button" name="button" @click="turnPlace(index)">交易</button></p>
+            <p><button type="button" name="button">放弃</button></p>
             <p><button type="button" name="button" @click="turnPlace(index)" style="background: #F2F2F2; color: #666; margin-top: 20px; width: 79px;">去接单</button></p>
-          </div>
+          </div> -->
         </el-col>
         <el-col :span="3" v-else-if="item.intentionStatus == '待接单'"><div class="mes operaMes">
             <!--<p><button type="button" name="button" @click="turnPlace(index)">交易</button></p>-->
